4b12fd934bd1344b2d6de0415b76a7048e0ee882
sarcho22 <schoi0427@gmail.com>
2024-12-10 16:24:43 -0500

    Implemented CalculateRate without filters
    need to implement filters once addfilter is complete

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainApplication {
     private static final String URL = "jdbc:postgresql://localhost:5432/mbs";
@@ -11,7 +14,6 @@ public class MainApplication {
     private static final String PASSWORD = "project2";
 
     private static final Scanner in = new Scanner(System.in);
-    // referenced https://mkyong.com/jdbc/how-do-connect-to-postgresql-with-jdbc-driver-java/ to establish connection
     public static void main(String[] args) {
         System.out.println("Welcome! [insert some generic msg or we can name this silly app]");
 
@@ -36,20 +38,27 @@ public class MainApplication {
                     System.out.println("Invalid option. Please try again (enter a number)!");
             }
         }
+    }
 
-        // try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+    // referenced https://mkyong.com/jdbc/how-do-connect-to-postgresql-with-jdbc-driver-java/ to establish connection
+    private static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            // debugging statements
+            // if (conn != null) {
+            //     System.out.println("Connected to the database!");
+            // } else {
+            //     System.out.println("Failed to make connection!");
+            // }
+            return conn;
 
-        //     if (conn != null) {
-        //         System.out.println("Connected to the database!");
-        //     } else {
-        //         System.out.println("Failed to make connection!");
-        //     }
-
-        // } catch (SQLException e) {
-        //     System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     // display the menu for the three functionalities
@@ -153,6 +162,124 @@ public class MainApplication {
     }
 
     private static void calculateRate() {
-        System.out.println("calculate rate to be implemented...");
+        // NEED TO UPDATE CALCULATE RATE ONCE ADD FILTER IS WORKING!!!
+
+        String query = """
+            SELECT a.loan_amount_000s, a.rate_spread, a.lien_status, a.application_id
+            FROM application a JOIN ActionTaken at ON a.action_taken = at.action_taken
+            WHERE at.action_taken = 1 AND a.purchaser_type IN (0, 1, 2, 3, 4, 8);
+            """;
+
+        try {
+            Connection conn = getConnection();
+            if(conn == null) {
+                System.out.println("Database connection failed...");
+                return;
+            }
+
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            // variables to calculate weighted avg of rates (weighted by loan amt) AND sum of all mortgages as cost of securitization
+            double weightedRateSum = 0.0;
+            long loanSum = 0;
+
+            // making a list of application_ids to know which ones to update if user accepts
+            ArrayList<Integer> applicationIds = new ArrayList<>();
+
+            while(rs.next()) {
+                // multiplied loan amount by 1000s because it was stored in the database in 1000s!!!
+                int loanAmount = rs.getInt("loan_amount_000s") * 1000;
+                int lienStatus = rs.getInt("lien_status");
+
+                double rateSpread = rs.getDouble("rate_spread");
+
+                // checked https://docs.oracle.com/javase/8/docs/api/java/sql/ResultSet.html to see how to determine if a value is null
+                if(rs.wasNull()) {
+                    if(lienStatus == 1) {
+                        rateSpread = 1.5;
+                    }
+                    else{
+                        rateSpread = 3.5;
+                    }
+
+                }
+
+                rateSpread += 2.33; // account for base rate!!
+
+                loanSum += loanAmount;
+                weightedRateSum += rateSpread * loanAmount;
+
+                applicationIds.add(rs.getInt("application_id"));
+            }
+
+            if(loanSum > 0) { // just to make sure no division by 0
+                // to fully understand how the weighted avg rate works, i used this site:
+                // https://www.savingforcollege.com/article/how-to-calculate-the-weighted-average-interest-rate
+                double weightedRateAvg = weightedRateSum / loanSum;
+                System.out.println("\n=-=-=-=-Rate Calculation-=-=-=-=");
+                System.out.printf("   Total Loan Amount: %d\n   Weighted Average Rate: %.3f\n", loanSum, weightedRateAvg);
+
+                System.out.print("\nDo you accept this rate? (yes/no): ");
+                String response = in.nextLine().trim().toLowerCase();
+
+                if(response.equals("yes")) {
+                    updateDatabase(conn, applicationIds);
+                }
+                else if(response.equals("no")) {
+                    System.out.println("Rate declined. Returning to main menu.\n\n");
+                }
+            }
+            else if(loanSum < 0) {
+                System.out.println("Integer Overflow Error!");
+            }
+            else {
+                System.out.println("No matching mortgages found.");
+            }
+
+        } catch (Exception e) {
+            System.err.println("An error occurred during rate calculation: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateDatabase(Connection conn, ArrayList<Integer> ids) {
+        // used https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html to figure out how to use parameters in a query in jdbc
+        String query = """
+                UPDATE Application
+                SET purchaser_type = 5
+                WHERE application_id = ?;
+                """;
+        
+        try {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            // used https://www.geeksforgeeks.org/how-to-execute-multiple-sql-commands-on-a-database-simultaneously-in-jdbc/ to figure out how to run multiple queries instead of running each one individually
+            // also used https://docs.raima.com/rdm/15_2/ug/jdbc/Statement/Method/executeBatch.htm to figure out how to determine if the updates were successful or failed
+
+            for(int id : ids) {
+                stmt.setInt(1, id);
+                stmt.addBatch();
+            }
+            int[] updateStatus = stmt.executeBatch();
+            int count = 0;
+            ArrayList<Integer> failed = new ArrayList<Integer>();
+
+            for(int i = 0; i < updateStatus.length; i++) {
+                if(updateStatus[i] == Statement.EXECUTE_FAILED) {
+                    failed.add(ids.get(i));
+                }
+                else {
+                    count++;
+                }
+            }
+
+            System.out.printf("Updated %d loans to 'Private Securitization'.\n", count);
+            if(!failed.isEmpty()) {
+                System.err.println("Failed to update the following application IDsP: " + failed);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Failed to update database: " + e.getMessage());
+        }
     }
 }
