6e97f133d721a8035e5e77f9d04788386509f56f
sarcho22 <schoi0427@gmail.com>
2024-12-10 22:18:23 -0500

    changed addfilter
    updated the way we process filters and did the display; now need to check for active filters and build dynamic query based on that

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -16,10 +16,15 @@ public class MainApplication {
     private static final String PASSWORD = "project2";
 
     private static final Scanner in = new Scanner(System.in);
-    private static List<County> allCounties;
+    // private static List<County> allCounties;
+    private static Map<String, List<String>> currentFilters = new HashMap<>();
+    private static Map<String, List<String>> allOptions = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println("Welcome! [insert some generic msg or we can name this silly app]");
-        allCounties = retrieveCounties();
+        initFilterOptions();
+
+        System.out.println("Welcome to the Mortgage Backed Security Packer!");
+        // allCounties = retrieveCounties();
 
         while(true) {
             displayMenu();
@@ -78,36 +83,33 @@ public class MainApplication {
         System.out.print("Choose an option (enter a number): ");
     }
 
-    private static List<County> retrieveCounties() {
-        List<County> counties = new ArrayList<>();
+    // private static List<County> retrieveCounties() {
+    //     List<County> counties = new ArrayList<>();
 
-        String query = "SELECT county_code, county_name FROM county";
+    //     String query = "SELECT county_code, county_name FROM county";
 
-        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             PreparedStatement stmt = conn.prepareStatement(query);
-             ResultSet rs = stmt.executeQuery()) {
+    //     try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    //          PreparedStatement stmt = conn.prepareStatement(query);
+    //          ResultSet rs = stmt.executeQuery()) {
 
-            while (rs.next()) {
-                String countyCode = rs.getString("county_code");
-                String countyName = rs.getString("county_name");
-                counties.add(new County(countyCode, countyName));
-            }
-        } catch (SQLException e)
-        {
-            System.err.println("Error fetching counties: " + e.getMessage());
-        }
-        return counties;
-    }
-
-
-    private static Map<String, List<String>> currentFilters = new HashMap<>();
+    //         while (rs.next()) {
+    //             String countyCode = rs.getString("county_code");
+    //             String countyName = rs.getString("county_name");
+    //             counties.add(new County(countyCode, countyName));
+    //         }
+    //     } catch (SQLException e)
+    //     {
+    //         System.err.println("Error fetching counties: " + e.getMessage());
+    //     }
+    //     return counties;
+    // }
 
     private static void addFilter() {
-        System.out.println("add filter NOT fully implemented (only worked on display, not functionality)...");
-        System.out.println("Choose a filter type:");
+        // add filter NOT fully implemented (only worked on display, not functionality)
+        System.out.println("=-=-=-=-Filter Types-=-=-=-=");
         // MAKE SURE TO REMOVE OPTION IF WE DONT IMPLEMENT ALL OF THESE
         System.out.println("1. MSAMD\n2. Income to Debt Ratio\n3. County\n4. Loan Type\n5. Tract to MSAMD Income\n6. Loan Purpose\n7. Property Type\n");
-        System.out.println("Which filter would you like to add (enter a number)?");
+        System.out.println("\nWhich filter would you like to add (enter a number)?");
         String option = in.nextLine();
 
         switch(option) {
@@ -115,99 +117,198 @@ public class MainApplication {
                 // provide a list of msamd with names if they have them, or numbers if there
                 // is no name, the user should be able to pick any one or more of them. (allow list of
                 // acceptable MSAMD)
-                addingSpecFilter("MSAMD", "Enter MSAMD(s), type -1 when the list is complete:");
+                // addingSpecFilter("MSAMD", "Enter MSAMD(s), type -1 when the list is complete:");
+                addListFilter("MSAMD");
                 break;
             case "2": 
                 // (for this project you can use
                 // applicant income / loan amount to compute this ratio) 
-                System.out.print("Enter minimum income-to-debt ratio: ");
-                double minIncomeToDebtRatio = in.nextDouble();
-                System.out.print("Enter maximum income-to-debt ratio: ");
-                double maxIncomeToDebtRatio = in.nextDouble();
-                in.nextLine();
+                // System.out.print("Enter minimum income-to-debt ratio: ");
+                // double minIncomeToDebtRatio = in.nextDouble();
+                // System.out.print("Enter maximum income-to-debt ratio: ");
+                // double maxIncomeToDebtRatio = in.nextDouble();
+                // in.nextLine();
 
-                List<String> filters1 = Arrays.asList(String.valueOf(minIncomeToDebtRatio), String.valueOf(maxIncomeToDebtRatio));
-                currentFilters.put("Income to Debt Ratio", filters1);
-                System.out.println("Added Income to Debt Ratio filter: " + minIncomeToDebtRatio + " to " + maxIncomeToDebtRatio);
+                // List<String> filters1 = Arrays.asList(String.valueOf(minIncomeToDebtRatio), String.valueOf(maxIncomeToDebtRatio));
+                // currentFilters.put("Income to Debt Ratio", filters1);
+                // System.out.println("Added Income to Debt Ratio filter: " + minIncomeToDebtRatio + " to " + maxIncomeToDebtRatio);
+                addRangeFilter("Income to Debt Ratio", "Enter minimum income-to-debt ratio:", "Enter maximum income-to-debt ratio:");
                 break;
             case "3":
-                addCountyFilter();
-                //addingSpecFilter("County", "Enter County Name(s), type -1 when the list is complete:");
+
+                // addCountyFilter();
+                // addingSpecFilter("County", "Enter County Name(s), type -1 when the list is complete:");
+                addListFilter("County");
                 break;
             case "4":
-                addingSpecFilter("Loan Type", "Enter Loan Type(s), type -1 when the list is complete: ");
+                // addingSpecFilter("Loan Type", "Enter Loan Type(s), type -1 when the list is complete: ");
+                addListFilter("Loan Type");
                 break;
             case "5":
-                System.out.print("Enter minimum tract to MSAMD income ratio: ");
-                double minTractRatio = in.nextDouble();
-                System.out.print("Enter maximum tract to MSAMD income ratio: ");
-                double maxTractRatio = in.nextDouble();
-                in.nextLine();
+                // System.out.print("Enter minimum tract to MSAMD income ratio: ");
+                // double minTractRatio = in.nextDouble();
+                // System.out.print("Enter maximum tract to MSAMD income ratio: ");
+                // double maxTractRatio = in.nextDouble();
+                // in.nextLine();
 
-                List<String> filters2 = Arrays.asList(String.valueOf(minTractRatio), String.valueOf(maxTractRatio));
-                currentFilters.put("Tract to MSAMD Income", filters2);
-                System.out.println("Added Tract to MSAMD Income range: " + minTractRatio + " to " + maxTractRatio);
+                // List<String> filters2 = Arrays.asList(String.valueOf(minTractRatio), String.valueOf(maxTractRatio));
+                // currentFilters.put("Tract to MSAMD Income", filters2);
+                // System.out.println("Added Tract to MSAMD Income range: " + minTractRatio + " to " + maxTractRatio);
+                addRangeFilter("Tract to MSAMD Income", "Enter minimum tract to MSAMD income ratio:", "Enter maximum tract to MSAMD income ratio:");
                 break;
             
             case "6":
-                addingSpecFilter("Loan Purpose", "Enter Loan Purpose(s), type -1 when the list is complete: ");
+                // addingSpecFilter("Loan Purpose", "Enter Loan Purpose(s), type -1 when the list is complete: ");
+                addListFilter("Loan Type");
                 break;
             case "7":
-                addingSpecFilter("Property Type", "Enter Property Type(s), type -1 when the list is complete: ");
+                // addingSpecFilter("Property Type", "Enter Property Type(s), type -1 when the list is complete: ");
+                addListFilter("Property Type");
                 break;
             default:
-                System.out.println("Invalid option!");
+                System.out.println("Invalid option! Back to main menu.");
                 break;
         }
         
     }
 
-    private static void addCountyFilter()
-    {
-        if(allCounties.isEmpty()){
-            System.out.println("There are no counties left to select!");
+    private static void addListFilter(String filter) {
+        List<String> available = new ArrayList<>(allOptions.get(filter));
+        List<String> selected = currentFilters.get(filter);
+
+        if(selected != null) {
+            // found "removeall" function in https://docs.oracle.com/javase/8/docs/api/java/util/List.html
+            available.removeAll(selected);
+        }
+        else {
+            selected = new ArrayList<>();
+        }
+
+        if(available.isEmpty()) {
+            System.out.printf("No more options available for %s.\n", filter);
             return;
         }
-        System.out.println("Available Counties:");
-        for(int i=0;i<allCounties.size();i++){
-            System.out.println((i)+". "+ allCounties.get(i).getCountyName());
-        }
-        System.out.println("Select counties by their number (separate multiple choices with commas!)");
-        String selection = in.nextLine();
-        String [] selectionIndex = selection.split(",");
-        List<County> temp = new ArrayList<>();
-        List<String> selectedCounties = new ArrayList<>();
-        boolean removed = false;
-        for(int i=0;i<retrieveCounties().size();i++){
-            for (String index : selectionIndex) {
-                if (Integer.parseInt(index)==i) {
-                    selectedCounties.add(allCounties.get(i).getCountyName());
-                    removed = true;
-                }
-            }
-            if(!removed)
-            {
-                temp.add(new County(allCounties.get(i).getCountyCode(), allCounties.get(i).getCountyName()));
-            }
-            removed = false;
-        }
-        allCounties = temp;
-        currentFilters.put("County", selectedCounties);
 
-    }
+        // display option menu
+        System.out.printf("Available options for %s:\n", filter);
+        for(int i = 0; i < available.size(); i++) {
+            System.out.printf("%d. %s\n", (i+1), available.get(i));
+        }
 
-    private static void addingSpecFilter(String filterName, String output) {
-        System.out.println(output);
-        List<String> filters = new ArrayList<>();
+        System.out.println("Which option would you like to filter for (enter numbers separated by commas: \"1,2,3\")?");
         String input = in.nextLine();
-        while(!input.equals("-1")){
-            filters.add(input);
-            System.out.println("Enter another " + filterName + " or -1 to finish.");
-            input = in.nextLine();
+        String[] inputs = input.split(",");
+
+        boolean failed = false;
+        List<String> success = new ArrayList<>();
+
+        for(int i = 0; i < inputs.length; i++) {
+            String choice = inputs[i].trim();
+            try {
+                int index = Integer.parseInt(choice) - 1; // -1 because the options start counting from 1 but indices start as 0
+                if(index >= 0 && index < available.size()) {
+                    String op = available.get(index);
+                    if(!selected.contains(op)) {
+                        selected.add(op);
+                        success.add(op);
+                    }
+                    else {
+                        failed = true;
+                    }
+                }
+                else {
+                    failed = true;
+                }
+            } catch (Exception e) {
+                failed = true; // invalid!!
+            }
         }
-        currentFilters.put(filterName, filters);
-        System.out.println("Added: " + filterName + " filter for: " + String.join(", ",filters));
+
+        if(!selected.isEmpty()) {
+            currentFilters.put(filter, selected);
+        }
+
+        if(!success.isEmpty()) {
+            System.out.println("Successfully added " + filter + " filter(s): " + String.join(", ", success));
+            if(failed) {
+                System.out.println("The rest of the filters couldn't be applied.");
+            }
+        }
+        else {
+            System.out.println("No filters were successfully applied.");
+        }
     }
+
+    private static void addRangeFilter(String filter, String minString, String maxString) {
+        try {
+            System.out.println(minString);
+            double min = Double.parseDouble(in.nextLine().trim());
+            System.out.println(maxString);
+            double max = Double.parseDouble(in.nextLine().trim());
+
+            if(min > max) {
+                System.out.println("Min can't be greater than max! Returning to main menu.");
+            }
+            else if(min < 0 || max < 0) {
+                System.out.println("No negative numbers! Returning to main menu.");
+            }
+            else {
+                List<String> range = Arrays.asList("" + min, "" + max);
+                currentFilters.put(filter, range);
+                System.out.printf("Added %s filter: between %f and %f.", filter, min, max);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Invalid input! Returning to main menu.");
+        }
+    }
+
+    // private static void addCountyFilter()
+    // {
+    //     if(allCounties.isEmpty()){
+    //         System.out.println("There are no counties left to select!");
+    //         return;
+    //     }
+    //     System.out.println("Available Counties:");
+    //     for(int i=0;i<allCounties.size();i++){
+    //         System.out.println((i)+". "+ allCounties.get(i).getCountyName());
+    //     }
+    //     System.out.println("Select counties by their number (separate multiple choices with commas!)");
+    //     String selection = in.nextLine();
+    //     String [] selectionIndex = selection.split(",");
+    //     List<County> temp = new ArrayList<>();
+    //     List<String> selectedCounties = new ArrayList<>();
+    //     boolean removed = false;
+    //     for(int i=0;i<retrieveCounties().size();i++){
+    //         for (String index : selectionIndex) {
+    //             if (Integer.parseInt(index)==i) {
+    //                 selectedCounties.add(allCounties.get(i).getCountyName());
+    //                 removed = true;
+    //             }
+    //         }
+    //         if(!removed)
+    //         {
+    //             temp.add(new County(allCounties.get(i).getCountyCode(), allCounties.get(i).getCountyName()));
+    //         }
+    //         removed = false;
+    //     }
+    //     allCounties = temp;
+    //     currentFilters.put("County", selectedCounties);
+
+    // }
+
+    // private static void addingSpecFilter(String filterName, String output) {
+    //     System.out.println(output);
+    //     List<String> filters = new ArrayList<>();
+    //     String input = in.nextLine();
+    //     while(!input.equals("-1")){
+    //         filters.add(input);
+    //         System.out.println("Enter another " + filterName + " or -1 to finish.");
+    //         input = in.nextLine();
+    //     }
+    //     currentFilters.put(filterName, filters);
+    //     System.out.println("Added: " + filterName + " filter for: " + String.join(", ",filters));
+    // }
 
     private static void displayActiveFilters()
     {
@@ -358,5 +459,64 @@ public class MainApplication {
         } catch (SQLException e) {
             System.err.println("Failed to update database: " + e.getMessage());
         }
+    }
+
+    private static void initFilterOptions() {
+        allOptions.put("MSAMD", Arrays.asList(
+            "Allentown, Bethlehem, Easton - PA, NJ",
+            "Atlantic City, Hammonton - NJ",
+            "Camden - NJ",
+            "Newark - NJ, PA",
+            "New York, Jersey City, White Plains - NY, NJ",
+            "35620",
+            "Ocean City - NJ",
+            "37980",
+            "Trenton - NJ",
+            "Vineland, Bridgeton - NJ",
+            "Wilmington - DE, MD, NJ"
+        ));
+
+        allOptions.put("County", Arrays.asList(
+            "Atlantic County",
+            "Cumberland County",
+            "Essex County",
+            "Gloucester County",
+            "Hudson County",
+            "Hunterdon County",
+            "Mercer County",
+            "Middlesex County",
+            "Monmouth County",
+            "Morris County",
+            "Ocean County",
+            "Bergen County",
+            "Passaic County",
+            "Salem County",
+            "Somerset County",
+            "Sussex County",
+            "Union County",
+            "Warren County",
+            "Burlington County",
+            "Camden County",
+            "Cape May County"
+        ));
+
+        allOptions.put("Loan Type", Arrays.asList(
+            "Conventional",
+            "FHA-insured",
+            "VA-guaranteed",
+            "FSA/RHS-guaranteed"
+        ));
+
+        allOptions.put("Loan Purpose", Arrays.asList(
+            "Home purchase",
+            "Home improvement",
+            "Refinancing"
+        ));
+
+        allOptions.put("Property Type", Arrays.asList(
+            "One-to-four family dwelling (other than manufactured housing)",
+            "Manufactured housing",
+            "Multifamily dwelling"
+        ));
     }
 }
